be830746ca3484794a6870638f024dab783094fa
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-23 17:55:52 +0200

    implement the photo cropping logic to retrieve the licence plate from the photo

--- car-plate-reading/car_plate_reading.py
+++ car-plate-reading/car_plate_reading.py
@@ -6,7 +6,6 @@ import easyocr
 
 img = cv2.imread('/Users/ekaterinastoyanova/PycharmProjects/Car-Plate-Recognition/images/picture1.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 # adding less sound to the photo so as to read the symbols better
 img_filter = cv2.bilateralFilter(gray, 11, 17, 17)
 # fix the contours
@@ -16,11 +15,25 @@ contour = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NON
 contour = imutils.grab_contours(contour)
 contour = sorted(contour, key=cv2.contourArea, reverse=True)[:8] # sort the contours in reverse to put them on top
 # getting only the last 8 contours
-
+CAR_PLATE_SIDES = 4
 # finding the contours that contain the car plate
 pos = None
 for c in contour:
-    approx = cv2.approxPolyDP(c, 5, True)
+    approx = cv2.approxPolyDP(c, 10, True)
+    if len(approx) == CAR_PLATE_SIDES:
+        # found a rectangle => car plate
+        pos = approx
+        break
+# using bitwise operations and masks to locate the car plate
+mask = np.zeros(gray.shape, np.uint8)
+new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
+bitwise_op = cv2.bitwise_and(img, img, mask=mask)
+# find out what the car plate says
+# cutting out the plate from the picture
+x,y = np.where(mask == 255) # retrieve the pixels
+x1, y1 = np.min(x), np.min(y) # get the starting coordinates
+x2, y2 = np.max(x), np.max(y)
+crop = gray[x1:x2, y1:y2]
 
-plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
 plt.show()
